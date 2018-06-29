90efb74d52148b4b4e31f96e5b35bc5f12cce6ea
Luke5Sky <luke.gangel87@gmail.com>
2018-06-29 14:56:40 +0200

    Initial upload of all files from development environment

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
-## What Can You Do
-Tells you what you can do
-
-## Description
-It tells you what you can do with mycroft (some examples) and lists the installed skills (if you want)
-
-## Examples
- - "What can you do?"
- - "What can i ask you?"
- - "What can i do?"
+To submit your skill, replace this file with text from 
+https://rawgit.com/MycroftAI/mycroft-skills/master/meta_editor.html
 
 
-## Credits
-Luke5Sky
+## YOUR SKILL NAME
+One line description of the skill
 
+## Description 
+A more verbose description, including any extra instructions or
+information that didn't fit in the one line.
 
+## Examples 
+* "Hello world"
+* "Greetings planet earth"
+* "Count up"
+* "Count down"
+
+## Credits 
+Your name
--- __init__.py
+++ __init__.py
@@ -1,16 +1,36 @@
+# TODO: Add an appropriate license to your skill before publishing.  See
+# the LICENSE file for more information.
+import os
+
 from adapt.intent import IntentBuilder
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft.skills.core import MycroftSkill, intent_handler
+from mycroft.util.log import LOG
 
+__author__ = 'luke5sky'
 
-class WhatCanYouDo(MycroftSkill):
+class WhatCanYouDoSkill(MycroftSkill):
+
     def __init__(self):
-        MycroftSkill.__init__(self)
-
-    @intent_file_handler('do.you.can.what.intent')
-    def handle_do_you_can_what(self, message):
-        self.speak_dialog('do.you.can.what')
+        super(WhatCanYouDoSkill, self).__init__(name="WhatCanYouDoSkill")
+        
+    @intent_handler(IntentBuilder("").require("What").require("Can").require("Do"))
+    def handle_what_can_do__intent(self, message):
+        self.speak_dialog("what.i.can")
+        myskills = os.popen('msm list | grep installed').read()
+        myskills = myskills.replace('\n', ', ').replace('\r', ', ').replace('[installed],', ',').replace('\t', '')
+        nr_skills = len(myskills.split())
+        nr_skills = 'Here we go, you have %d skills installed.' % nr_skills
+        self.speak(nr_skills)
+        should_getskills = self.get_response('ask.getskills')
+        yes_words = set(self.translate_list('yes'))
+        if should_getskills:
+            resp_getskills = should_getskills.split()
+            if any(word in resp_getskills for word in yes_words):
+               self.speak_dialog('my.skills')
+               self.speak(myskills.strip())
+            else:
+               self.speak_dialog('no.skills')
 
 
 def create_skill():
-    return WhatCanYouDo()
-
+    return WhatCanYouDoSkill()
