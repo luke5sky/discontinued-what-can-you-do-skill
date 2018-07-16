f97c0ac7aaf371d61fd22625915c21edede118f5
Luke5Sky <luke.gangel87@gmail.com>
2018-07-16 13:02:12 +0200

    inline documentation

--- __init__.py
+++ __init__.py
@@ -11,8 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-# TODO: Documentation and faster method to get list of installed skills
 
 import os
 
@@ -30,36 +28,37 @@ class WhatCanYouDoSkill(MycroftSkill):
         
     @intent_handler(IntentBuilder("").require("What").require("Can").require("Do"))
     def handle_what_can_do__intent(self, message):
-        self.speak_dialog("what.i.can")
-        self.getSkills()
+        self.speak_dialog("what.i.can") # tell user what he can do
+        self.getSkills() # execute function getSkills -> get list of installed skills
 
     def getSkills(self):
-        self.myskills = os.popen('msm list | grep installed').read()
-        self.myskills = self.myskills.replace('\n', ', ').replace('\r', ', ').replace('[installed],', ',').replace('\t', '')
-        nr_skills = len(self.myskills.split())
-        if nr_skills < 1:
-           self.myskills = os.popen('ls /opt/mycroft/skills/').read()
-           self.myskills = self.myskills.replace('\n', ', ').replace('\r', ', ').replace('\t', '')
-           nr_skills = len(self.myskills.split())
-        if nr_skills < 1:
-           wait_while_speaking()
+        self.myskills = os.popen('msm list | grep installed').read() # get list of skills via msm and search for "installed"
+        self.myskills = self.myskills.replace('\n', ', ').replace('\r', ', ').replace('[installed],', ',').replace('\t', '') # replace unwanted characters and make nice list
+        nr_skills = len(self.myskills.split()) # get number of skills
+        if nr_skills < 1: # if msm did not give us what we want (no matter why) do alternative skill search
+           self.myskills = os.popen('ls /opt/mycroft/skills/').read() # Get folders in /opt/mycroft/skills
+           self.myskills = self.myskills.replace('\n', ', ').replace('\r', ', ').replace('\t', '') # replace unwanted characters and make 
+nice list
+           nr_skills = len(self.myskills.split()) # get number of skills
+        if nr_skills < 1: # if msm and alternative skill search fails than tell user that we couldn't do the job
+           wait_while_speaking() # always wait
            self.speak_dialog("not.found")
-           return
-        wait_while_speaking()
-        self.speak_dialog('found', {'nrskills': nr_skills})
-        wait_while_speaking()
-        self.should_getskills = self.get_response('ask.getskills')
-        self.yes_words = set(self.translate_list('yes'))
-        self.listSkills()
+           return # if all fails, return
+        wait_while_speaking() # always wait
+        self.speak_dialog('found', {'nrskills': nr_skills}) # we found skills -> yeah. tell user how many!
+        wait_while_speaking() # always wait
+        self.should_getskills = self.get_response('ask.getskills') # ask user if we should give him a list of all his skills.
+        self.yes_words = set(self.translate_list('yes')) # get list of confirmation words
+        self.listSkills() # execute function listSkills -> if user confirmed -> give him a list of all his skills, else -> exit
       
     def listSkills(self):
-        if self.should_getskills:
-           resp_getskills = self.should_getskills.split()
-           if any(word in resp_getskills for word in self.yes_words):
-              self.speak_dialog('my.skills')
-              self.speak(self.myskills.strip())
-           else:
-              self.speak_dialog('no.skills')
+        if self.should_getskills: # if user said something
+           resp_getskills = self.should_getskills.split() # split user sentence into list
+           if any(word in resp_getskills for word in self.yes_words): # if any of the words from the user sentences is yes
+              self.speak_dialog('my.skills') # Introduction that we will give user list of skills
+              self.speak(self.myskills.strip()) # tell user list of skills
+           else: # no word in sentence from user was yes
+              self.speak_dialog('no.skills') # give user feedback
 
     def shutdown(self):
         super(WhatCanYouDoSkill, self).shutdown()
